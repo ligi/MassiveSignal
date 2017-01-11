6f7df7860e2f5ce91a023eba4adaac639192eeee
ligi <ligi@ligi.de>
2017-01-11 21:27:18 +0100

    Now with 2 oscillators

--- app/src/main/java/massivesignal/org/ligi/massivesignal/MainActivity.kt
+++ app/src/main/java/massivesignal/org/ligi/massivesignal/MainActivity.kt
@@ -6,6 +6,7 @@ import com.jsyn.JSyn
 import com.jsyn.devices.android.AndroidAudioForJSyn
 import com.jsyn.unitgen.LineOut
 import com.jsyn.unitgen.SineOscillator
+import com.jsyn.unitgen.SineOscillatorPhaseModulated
 import kotlinx.android.synthetic.main.activity_main.*
 import org.ligi.kaxt.doOnProgressChanged
 
@@ -17,26 +18,33 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
         synthesizer.start()
 
         val lineOut = LineOut()
         synthesizer.add(lineOut)
 
+        val carrier = SineOscillatorPhaseModulated()
+        synthesizer.add(carrier)
 
-        val sineOscillator = SineOscillator()
-        synthesizer.add(sineOscillator)
+        val modulator = SineOscillator()
+        synthesizer.add(modulator)
 
-        sineOscillator.output.connect(0,lineOut.input,0)
-        sineOscillator.output.connect(0,lineOut.input,1)
+        modulator.output.connect(carrier.modulation)
 
-        sineOscillator.frequency.set(400.0)
-        sineOscillator.amplitude.set(0.5)
+        modulator.amplitude.setup(0.0, 1.0, 10.0)
+        carrier.amplitude.setup(0.0, 1.0, 10.0)
+
+        carrier.output.connect(0, lineOut.input, 0)
+        carrier.output.connect(0, lineOut.input, 1)
 
         lineOut.start()
 
         parameterSeek.doOnProgressChanged { progress, fromUser ->
-            sineOscillator.frequency.set(.4*progress)
+            carrier.frequency.set(.4 * progress)
+        }
+
+        parameter2Seek.doOnProgressChanged { progress, fromUser ->
+            modulator.frequency.set(.4 * progress)
         }
     }
 
