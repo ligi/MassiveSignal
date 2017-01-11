519f263ba83d6736b1b2522a97d30310ba1647e7
ligi <ligi@ligi.de>
2017-01-11 20:27:39 +0100

    Initial tone generation with JSyn

--- app/src/main/java/massivesignal/org/ligi/massivesignal/MainActivity.kt
+++ app/src/main/java/massivesignal/org/ligi/massivesignal/MainActivity.kt
@@ -2,37 +2,47 @@ package massivesignal.org.ligi.massivesignal
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import com.jsyn.JSyn
+import com.jsyn.devices.android.AndroidAudioForJSyn
+import com.jsyn.unitgen.LineOut
+import com.jsyn.unitgen.SineOscillator
 import kotlinx.android.synthetic.main.activity_main.*
 import org.ligi.kaxt.doOnProgressChanged
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var sirenThread: SirenThread
+    private val synthesizer by lazy { JSyn.createSynthesizer(AndroidAudioForJSyn()) }
 
     public override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        sirenThread = SirenThread()
+
+        synthesizer.start()
+
+        val lineOut = LineOut()
+        synthesizer.add(lineOut)
+
+
+        val sineOscillator = SineOscillator()
+        synthesizer.add(sineOscillator)
+
+        sineOscillator.output.connect(0,lineOut.input,0)
+        sineOscillator.output.connect(0,lineOut.input,1)
+
+        sineOscillator.frequency.set(400.0)
+        sineOscillator.amplitude.set(0.5)
+
+        lineOut.start()
 
         parameterSeek.doOnProgressChanged { progress, fromUser ->
-            if (fromUser) sirenThread.parameter = progress / parameterSeek.max.toDouble()
+            sineOscillator.frequency.set(.4*progress)
         }
-
-        startButton.setOnClickListener {
-            if ( sirenThread.isRunning) {
-                sirenThread.isRunning = false
-                sirenThread = SirenThread()
-            } else {
-                sirenThread.start()
-            }
-        }
-
     }
 
     public override fun onDestroy() {
         super.onDestroy()
-        sirenThread.isRunning = false
+        synthesizer.stop()
     }
 
 }
